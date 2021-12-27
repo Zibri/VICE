ac61267613e5243bc435c5365b59a757d8ecfd40
Zibri <Zibri@users.noreply.github.com>
2021-12-27 17:24:02 +0200

    Update my_build.sh

--- my_build.sh
+++ my_build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
+#
+# Usage: github-actions-build.sh <UI> [SVN rXXXXX override, or 'release']
+
 set -o errexit
 set -o nounset
 cd "$(dirname $0)"/../..
 
 #
 # Build and install xa65. When this stops working, check
-# https://www.floodgap.com/retrotech/xa/dists/ for a newer version.
+# https://www.floodgap.com/retrotech/xa/dists/ to see why
 #
 
-XA_VERSION=2.3.12
+XA_VERSION=$(wget --tries=1 -O - https://www.floodgap.com/retrotech/xa/dists/ 2>/dev/null | grep '"xa-[^"]*gz"' | sed -e 's,.*xa-,,' -e 's,.tar.gz.*,,' | sort -V | tail -n1)
 
 if [ ! -e /usr/local/bin/xa65.exe ]
 then
@@ -22,6 +25,7 @@ then
     cp /usr/local/bin/xa.exe /usr/local/bin/xa65.exe
     popd
 fi
+
 
 ARGS="
     --disable-arch \
@@ -65,13 +69,22 @@ SDL2)
     ;;
 esac
 
-echo WE ARE IN DIRECTORY: $PWD
-apt install -y autoconf automake
-./autogen.sh
+# Skip autogen.sh when building release from tarball
+if [ "$2" = "release" ]; then
+    ./configure $ARGS || ( echo -e "\n**** CONFIGURE FAILED ****\n" ; cat config.log ; exit 1 )
+else
+    ./autogen.sh
+    ./configure $ARGS SVN_REVISION_OVERRIDE=$(echo "$2" | sed 's/^r//') || ( echo -e "\n**** CONFIGURE FAILED ****\n" ; cat config.log ; exit 1 )
+fi
+
 sed -i "s/O3/Ofast/g" configure.ac
 sed -i "s,Emulates an 8-bit Commodore computer.,Zibri Build,g" src/arch/gtk3/uiabout.c
 sed -i "s,GTK_LICENSE_GPL_2_0,GTK_LICENSE_UNKNOWN," src/arch/gtk3/uiabout.c
+
+make -j $(( $NUMBER_OF_PROCESSORS )) -s
+##### make bindistzip
+
 ##### sed -i "s,http://vice-emu.sourceforge.net/,\\\n\\\n         https://git.io/JCLMo\\\n\\\nhttp://vice-emu.sourceforge.net/," src/arch/gtk3/uiabout.c
-./configure $ARGS || ( echo -e "\n**** CONFIGURE FAILED ****\n" ; cat config.log ; exit 1 )
-make -j 8 -s
+#####./configure $ARGS || ( echo -e "\n**** CONFIGURE FAILED ****\n" ; cat config.log ; exit 1 )
+##### make -j 8 -s
 make bindist7zip
