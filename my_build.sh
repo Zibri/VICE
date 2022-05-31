c28e97fca3990957cabacc33e2587c57550bd7d4
Zibri <Zibri@users.noreply.github.com>
2022-05-31 17:44:02 +0200

    Update my_build.sh

--- my_build.sh
+++ my_build.sh
@@ -79,7 +79,7 @@ esac
 
 ./autogen.sh
 sed -i "s/O3/Ofast/g" configure.ac
-sed -i "s,Emulates an 8-bit Commodore computer.,Zibri Build,g" src/arch/gtk3/uiabout.c
+sed -i "s,Emulates 8-bit Commodore computers.,Zibri Build,g" src/arch/gtk3/uiabout.c
 sed -i "s,GTK_LICENSE_GPL_2_0,GTK_LICENSE_UNKNOWN," src/arch/gtk3/uiabout.c
 ##### sed -i "s,http://vice-emu.sourceforge.net/,\\\n\\\n         https://git.io/JCLMo\\\n\\\nhttp://vice-emu.sourceforge.net/," src/arch/gtk3/uiabout.c
 
