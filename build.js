b53f789f78ff22c65e50b4e284100bce8b7180d1
Zibri <Zibri@users.noreply.github.com>
2023-08-02 23:27:53 +0300

    Update build.js

--- build.js
+++ build.js
@@ -1,3 +1,6 @@
 var pty = require('node-pty');
 const { spawn } = require('child_process');
 const bat = spawn('cmd.exe', ['/c','setup.bat']);
+bat.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
