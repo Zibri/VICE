a71e098de2766b6b47e1c5ea53870b39f53ecebc
Zibri <Zibri@users.noreply.github.com>
2023-08-02 15:28:28 +0300

    Update vice.sh

--- vice.sh
+++ vice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd $GITHUB_WORKSPACE
 export PATH=$PATH:/usr/share:/usr/local/share:/mingw64/share:/runneradmin/.local/share
-pacman -Sy --noconfirm git base-devel autotools mingw-w64-x86_64-toolchain zip p7zip subversion git mingw-w64-x86_64-pkg-config mingw-w64-x86_64-ntldd mingw-w64-x86_64-glew mingw-w64-x86_64-giflib mingw-w64-x86_64-lame mingw-w64-x86_64-libvorbis mingw-w64-x86_64-flac mingw-w64-x86_64-icoutils mingw-w64-x86_64-gtk3  mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-ffmpeg4.4 msys/libpcap-devel mingw64/mingw-w64-x86_64-libpcap autotools
+pacman -Suy --noconfirm git base-devel autotools mingw-w64-x86_64-toolchain zip p7zip subversion git mingw-w64-x86_64-pkg-config mingw-w64-x86_64-ntldd mingw-w64-x86_64-glew mingw-w64-x86_64-giflib mingw-w64-x86_64-lame mingw-w64-x86_64-libvorbis mingw-w64-x86_64-flac mingw-w64-x86_64-icoutils mingw-w64-x86_64-gtk3  mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-ffmpeg4.4 msys/libpcap-devel mingw64/mingw-w64-x86_64-libpcap autotools
 wget https://mirror.cs.jmu.edu/pub/msys2/mingw/mingw64/mingw-w64-x86_64-ffmpeg-4.4.3-6-any.pkg.tar.zst
 wget https://mirror.cs.jmu.edu/pub/msys2/mingw/mingw64/mingw-w64-x86_64-celt-0.11.3-5-any.pkg.tar.zst
 pacman -U --noconfirm mingw-w64-x86_64-ffmpeg-4.4.3-6-any.pkg.tar.zst mingw-w64-x86_64-celt-0.11.3-5-any.pkg.tar.zst
-rm -rf *.zst
+
 XA_VERSION=$(wget --tries=1 -O - https://www.floodgap.com/retrotech/xa/dists/ 2>/dev/null | grep '"xa-[^"]*gz"' | sed -e 's,.*xa-,,' -e 's,.tar.gz.*,,' | sort -V | tail -n1)
 
 mkdir -p src
@@ -17,8 +17,11 @@ make mingw install
 ln -s /usr/local/bin/xa.exe /usr/local/bin/xa65.exe
 cd ../..
 rm -rf src
+echo "per IDDIO"
 
 svn checkout svn://svn.code.sf.net/p/vice-emu/code/trunk/vice vice
+
+ls -alR
 
 export USE_SVN_REVISION=1
 
