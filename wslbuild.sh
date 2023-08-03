f62b403234b3e830a5fd19b10976caf9d57630c4
Zibri <Zibri@users.noreply.github.com>
2023-08-04 01:40:19 +0300

    Update wslbuild.sh

--- wslbuild.sh
+++ wslbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y autoconf automake build-essential byacc devscripts dos2unix fakeroot flex xa65 7zip
+sudo apt install -y autoconf automake build-essential byacc devscripts dos2unix fakeroot flex xa65 p7zip-full
 sudo apt install -y libasound-dev libflac-dev libgif-dev libmp3lame-dev libmpg123-dev libpcap-dev libvorbis-dev libcurl4-openssl-dev
 sudo apt install -y libglew-dev libgtk-3-dev libpulse-dev subversion libavfilter-dev libavformat-dev libavcodec-dev
 
