6418e71c48a61fbf33e21864e818b041de0bbdce
Zibri <Zibri@users.noreply.github.com>
2023-08-04 00:08:09 +0300

    Update setupwsl.bat

--- setupwsl.bat
+++ setupwsl.bat
@@ -4,4 +4,6 @@ wsl --install -d ubuntu -n
 ubuntu install --root
 wsl --user root -e apt update
 wsl --user root -e apt upgrade -y
+cd %GITHUB_WORKSPACE%
+wsl --user root -e ls -al
 wsl --user root ./wslbuild.sh
