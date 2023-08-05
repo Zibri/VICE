963025b3fa590ce3f690624c835fb9a616e8a49e
Zibri <Zibri@users.noreply.github.com>
2023-08-05 05:24:10 +0300

    Create ossbuild.sh

---
+++ ossbuild.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+zypper -n in -t pattern devel_basis devel_C_C++
+zypper -n in 7zip fakeroot subversion gtk3-devel glew-devel ffmpeg-4-libavcodec-devel ffmpeg-4-libavfilter-devel ffmpeg-4-libavformat-devel ffmpeg-4-libavresample-devel libpulse-devel alsa-devel libvorbis-devel flac-devel giflib-devel
+
+sudo update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
+
+if [ ! -e /usr/local/bin/xa65 ]
+then
+  XA_VERSION=$(wget --tries=1 -O - https://www.floodgap.com/retrotech/xa/dists/ 2>/dev/null | grep '"xa-[^"]*gz"' | sed -e 's,.*xa-,,' -e 's,.tar.gz.*,,' | sort -V | tail -n1)
+  mkdir -p src
+  cd src
+  wget https://www.floodgap.com/retrotech/xa/dists/xa-${XA_VERSION}.tar.gz
+  tar -xzf xa-${XA_VERSION}.tar.gz
+  cd xa-${XA_VERSION}
+  make install
+  ln -s /usr/local/bin/xa /usr/local/bin/xa65
+  cd ../..
+  rm -rf src
+fi
+
+svn checkout svn://svn.code.sf.net/p/vice-emu/code/trunk/vice vice
+
+export USE_SVN_REVISION=1
+
+cd vice
+
+ARGS="--disable-arch --disable-pdf-docs --with-png --with-gif --with-vorbis --with-flac --enable-ethernet --enable-midi --enable-cpuhistory --enable-platformdox --enable-html-docs --enable-rs232 --enable-new8580filter --with-resid --enable-x64 --enable-x64-image --enable-realdevice --enable-ffmpeg --enable-gtk3ui"
+
+./autogen.sh
+./configure $ARGS
+sed -i "s/The %s Emulator/ð©ð²ð«ð»ð²'ð¼ ðð¾ð²ðµð­./" src/arch/gtk3/uiabout.c
+make -j8 DESTDIR=../data/build install-strip
+rev=$(svnversion)
+cd data/build/
+7z a ../../gtk3vice_3.7.1_${rev}.7z *
