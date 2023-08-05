64ac3f76b0d199eceacc3a5aa91432680c7621db
Zibri <Zibri@users.noreply.github.com>
2023-08-05 05:42:21 +0300

    Update osssetup.bat

--- osssetup.bat
+++ osssetup.bat
@@ -4,7 +4,6 @@ wsl --install -d openSUSE-Tumbleweed -n
 "C:\Program Files\Git\usr\bin\timeout.exe" >NUL: 40s openSUSE-Tumbleweed >NUL: run
 wsl zypper refresh
 cd %GITHUB_WORKSPACE%
-wsl --user root -e ls -al
 dos2unix ossbuild.sh
 dos2unix wsl.conf
 wsl --user root cp wsl.conf /etc/wsl.conf
