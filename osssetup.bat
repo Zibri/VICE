8c2c8e9e0e3f64395f5409dd20c5264082be73bb
Zibri <Zibri@users.noreply.github.com>
2023-08-05 13:55:56 +0300

    Update osssetup.bat

--- osssetup.bat
+++ osssetup.bat
@@ -1,7 +1,8 @@
 @echo off
 wsl --update
 wsl --install -d openSUSE-Tumbleweed -n
-"C:\Program Files\Git\usr\bin\timeout.exe" >NUL: 60s openSUSE-Tumbleweed >NUL: run
+rem "C:\Program Files\Git\usr\bin\timeout.exe" >NUL: 60s openSUSE-Tumbleweed >NUL: run
+node -e "const{spawn}=require('child_process');bat=spawn('openSUSE-Tumbleweed',['--install','']);bat.stdout.on('data',function(data){if(data.indexOf('YaST')!=-1){bat.kill();}});bat.stderr.on('data',function(data){if(data.indexOf('YaST')!=-1){bat.kill();}});"
 wsl --user root zypper refresh
 cd %GITHUB_WORKSPACE%
 dos2unix ossbuild.sh
