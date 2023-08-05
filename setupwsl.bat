4ef2085f75476303c6151621a345805d92ae139a
Zibri <Zibri@users.noreply.github.com>
2023-08-05 10:56:41 +0300

    Update setupwsl.bat

--- setupwsl.bat
+++ setupwsl.bat
@@ -4,11 +4,11 @@ rem wsl --install -d ubuntu -n
 rem ubuntu install --root
 wsl --install Ubuntu-20.04 -n
 ubuntu2004 install --root
-wsl -d "Ubuntu-20.04" --user root -e apt update
-wsl -d "Ubuntu-20.04" --user root -e apt upgrade -y
+wsl -d Ubuntu-20.04 --user root -e apt update
+wsl -d Ubuntu-20.04 --user root -e apt upgrade -y
 cd %GITHUB_WORKSPACE%
 dos2unix wslbuild.sh
 dos2unix wsl.conf
-wsl -d "Ubuntu-20.04" --user root cp wsl.conf /etc/wsl.conf
-wsl -d "Ubuntu-20.04" --shutdown
-wsl -d "Ubuntu-20.04" --user root ./wslbuild.sh
+wsl -d Ubuntu-20.04 --user root cp wsl.conf /etc/wsl.conf
+wsl -d Ubuntu-20.04 --shutdown
+wsl -d Ubuntu-20.04 --user root ./wslbuild.sh
