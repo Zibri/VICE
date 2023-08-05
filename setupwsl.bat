3133df307bf3ef91cf55c1bd7ccd26de5da4aab2
Zibri <Zibri@users.noreply.github.com>
2023-08-05 10:53:32 +0300

    Update setupwsl.bat

--- setupwsl.bat
+++ setupwsl.bat
@@ -1,5 +1,4 @@
 @echo off
-set GITHUB_WORKSPACE=%cd%
 wsl --update
 rem wsl --install -d ubuntu -n
 rem ubuntu install --root
@@ -7,7 +6,7 @@ wsl --install Ubuntu-20.04 -n
 ubuntu2004 install --root
 wsl -d "Ubuntu-20.04" --user root -e apt update
 wsl -d "Ubuntu-20.04" --user root -e apt upgrade -y
-rem cd %GITHUB_WORKSPACE%
+cd %GITHUB_WORKSPACE%
 dos2unix wslbuild.sh
 dos2unix wsl.conf
 wsl -d "Ubuntu-20.04" --user root cp wsl.conf /etc/wsl.conf
