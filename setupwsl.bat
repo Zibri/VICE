6d7c0d808ea487e8122ea8e223129fab9605d806
Zibri <Zibri@users.noreply.github.com>
2023-08-06 10:01:30 +0300

    Update setupwsl.bat

--- setupwsl.bat
+++ setupwsl.bat
@@ -7,6 +7,7 @@ rem wsl --user root -e apt upgrade -y
 cd %GITHUB_WORKSPACE%
 dos2unix wslbuild.sh
 dos2unix wsl.conf
+dos2unix installer.sh
 wsl --user root cp wsl.conf /etc/wsl.conf
 wsl --shutdown
 wsl --user root ./wslbuild.sh
