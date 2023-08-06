880746e006d1fccaad1746d3ed24801da169ab4b
Zibri <Zibri@users.noreply.github.com>
2023-08-06 16:05:13 +0300

    Update osssetup.bat

--- osssetup.bat
+++ osssetup.bat
@@ -8,6 +8,7 @@ cd %GITHUB_WORKSPACE%
 dos2unix ossbuild.sh
 dos2unix wsl.conf
 dos2unix installer.sh
+dos2unix vice_update_opensuse.sh
 wsl --user root cp wsl.conf /etc/wsl.conf
 wsl --shutdown
 wsl --user root ./ossbuild.sh
