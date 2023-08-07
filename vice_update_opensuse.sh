9ff2a7e6ccddd6167582351eb9ff02035e3fc68c
Zibri <Zibri@users.noreply.github.com>
2023-08-07 12:37:53 +0300

    Update vice_update_opensuse.sh

--- vice_update_opensuse.sh
+++ vice_update_opensuse.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-upd=$(curl -qs https://api.github.com/repos/Zibri/VICE/releases|grep \"tag_name\"|grep o|tr -d \"_tagname:\ r\\\",\"|sort -nr|head -1|sed s/o/M/)
+upd=$(curl -qs https://api.github.com/repos/Zibri/VICE/releases|grep \"tag_name\"|grep o|tr -d \"_tagname:\ r\\\",\"|sort -nr|head -1|tr -dc '0-9')
 if ! [ "$(which x64sc)" ]
 then
   myver="none"
 else
-  myver=$(strings $(which x64sc)|grep -B 1 "Current VICE"|head -n 1)
+  myver=$(strings $(which x64sc)|grep -B 1 "Current VICE"|head -n 1|tr -dc '0-9')
 fi
-if [[ "$upd" = "$myver" ]]
+if [[ "$upd" > "$myver" ]]
 then
-  echo No new version.
-else
   echo "Downloading VICE latest version..."
   dlurl=$(curl -qs 'https://api.github.com/repos/Zibri/VICE/releases'|grep download_url|grep gtk|grep 'o/'|sort -nr|head -1|cut -d'"' -f 4)
   echo "Downloading update $upd..."
@@ -17,5 +15,6 @@ else
   chmod a+x /tmp/viceup.sh
   sudo /tmp/viceup.sh -u
   sudo /tmp/viceup.sh
+else
+  echo No new version.
 fi
-
