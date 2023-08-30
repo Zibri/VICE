663ae543b64219251411c8c16ac31306b023dea3
Zibri <Zibri@users.noreply.github.com>
2023-08-30 15:53:07 +0300

    Update ossbuild.sh

--- ossbuild.sh
+++ ossbuild.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+zypper refresh
 zypper -n in -t pattern devel_basis devel_C_C++
-zypper -n in 7zip fakeroot subversion gtk3-devel glew-devel ffmpeg-4-libavcodec-devel ffmpeg-4-libavfilter-devel ffmpeg-4-libavformat-devel ffmpeg-4-libavresample-devel libpulse-devel alsa-devel libvorbis-devel flac-devel giflib-devel
+zypper -n in 7zip file wget curl dos2unix fakeroot subversion gtk3-devel glew-devel ffmpeg-4-libavcodec-devel ffmpeg-4-libavfilter-devel ffmpeg-4-libavformat-devel ffmpeg-4-libavresample-devel libpulse-devel alsa-devel libvorbis-devel flac-devel giflib-devel
 
 sudo update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
+
+cd /t
 
 if [ ! -e /usr/local/bin/xa65 ]
 then
@@ -36,5 +39,5 @@ cp -R data/data/build/usr/local/share data/build/usr/local/
 cd data/build/
 7z a ../../gtk3vice_${rev}.7z *
 cp ../../../vice_update_opensuse.sh ../../vice_update.sh
-cp ../../../installer.sh ../../gtk3vice_${rev}.sh
-cat ../../gtk3vice_${rev}.7z >>../../gtk3vice_${rev}.sh
+cp ../../../installer.sh ../../../gtk3vice_${rev}.sh
+cat ../../gtk3vice_${rev}.7z >>../../../gtk3vice_${rev}.sh
