ed4e6b1419f75204000c98726ebe8cd1eb99322b
Zibri <Zibri@users.noreply.github.com>
2023-08-30 16:03:34 +0300

    Update osssetup.sh

--- osssetup.sh
+++ osssetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull opensuse/tumbleweed
-docker run -t -i --volume .:/t opensuse/tumbleweed /t/ossbuild.sh
+docker run --volume .:/t opensuse/tumbleweed /t/ossbuild.sh
 docker rm $(docker ps -aq)
