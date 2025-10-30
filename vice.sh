467ab6d7107ad5d8228806a08b94c7afeca7a2f8
Robert Sinclair <0wwafa@gmail.com>
2025-10-30 07:44:09 +0300

    Update vice.sh

--- vice.sh
+++ vice.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export MSYSTEM=MINGW64
 cd $GITHUB_WORKSPACE
-pacman -Sy --noconfirm git base-devel autotools mingw-w64-x86_64-toolchain zip p7zip subversion git mingw-w64-x86_64-pkg-config mingw-w64-x86_64-ntldd mingw-w64-x86_64-glew mingw-w64-x86_64-giflib mingw-w64-x86_64-lame mingw-w64-x86_64-libvorbis mingw-w64-x86_64-flac mingw-w64-x86_64-icoutils mingw-w64-x86_64-gtk3  mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-ffmpeg4.4 msys/libpcap-devel mingw64/mingw-w64-x86_64-libpcap autotools
+pacman -Sy --noconfirm git base-devel autotools mingw-w64-x86_64-toolchain zip p7zip subversion git mingw-w64-x86_64-pkg-config mingw-w64-x86_64-ntldd mingw-w64-x86_64-glew mingw-w64-x86_64-giflib mingw-w64-x86_64-lame mingw-w64-x86_64-libvorbis mingw-w64-x86_64-flac mingw-w64-x86_64-icoutils mingw-w64-x86_64-gtk3 mingw-w64-x86_64-SDL2_image msys/libpcap-devel mingw64/mingw-w64-x86_64-libpcap autotools
+wget https://mirror.cs.jmu.edu/pub/msys2/mingw/mingw64/mingw-w64-x86_64-ffmpeg4.4-4.4.6-7-any.pkg.tar.zst
 wget https://mirror.cs.jmu.edu/pub/msys2/mingw/mingw64/mingw-w64-x86_64-celt-0.11.3-5-any.pkg.tar.zst
-pacman -U --noconfirm mingw-w64-x86_64-celt-0.11.3-5-any.pkg.tar.zst
-pacman -Sy --noconfirm mingw-w64-x86_64-ffmpeg4.4 mingw-w64-x86_64-xa65 mingw-w64-x86_64-libpcap mingw-w64-x86_64-curl
+pacman -U --noconfirm mingw-w64-x86_64-celt-0.11.3-5-any.pkg.tar.zst ./mingw-w64-x86_64-ffmpeg4.4-4.4.6-7-any.pkg.tar.zst
+pacman -Sy --noconfirm mingw-w64-x86_64-xa65 mingw-w64-x86_64-libpcap mingw-w64-x86_64-curl
 
 svn checkout svn://svn.code.sf.net/p/vice-emu/code/trunk/vice vice
 
